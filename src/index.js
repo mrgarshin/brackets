774f5c78b9b8597ab8ef7404d7d80163e9fd082f
mrgarshin <garshin.serg@yandex.ru>
2023-09-14 14:24:38 +0300

    feat: add solution (but not  perfect)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(element => {
+      if(str[i] === element[0]) {
+        stack.push(str[i])
+      } if(str[i] === element[1]) {
+        stack.pop();
+      }
+    });
+  }
+  return stack.length === 0;
 }
