402ef1f099f172281e0344963dc239536fa03cc7
mrgarshin <garshin.serg@yandex.ru>
2023-09-14 14:45:24 +0300

    fix: pass the fifth test

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ module.exports = function check(str, bracketsConfig) {
     bracketsConfig.forEach(element => {
       if(str[i] === element[0]) {
         stack.push(str[i])
-      } if(str[i] === element[1]) {
+      } if(str[i] === element[1] && stack[stack.length-1] === element[0]) {
         stack.pop();
       }
     });
   }
+  // console.log(stack)
   return stack.length === 0;
 }
+
+// const config2 = [['1', '2'], ['3', '4']];
+// check('1324', config2)
